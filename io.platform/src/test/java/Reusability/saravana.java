22b994b0f2d3f85cddc83867eb63e7734a21109f
saravanakumarsubramani <amsaravana96@gmail.com>
2024-03-21 16:45:19 +0530

    third commit

--- io.platform/src/test/java/Reusability/saravana.java
+++ io.platform/src/test/java/Reusability/saravana.java
@@ -5,7 +5,7 @@ public class saravana {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("saravana");
-		System.out.println("kishore");
+		System.out.println("kishor");
 		
 	}
 
